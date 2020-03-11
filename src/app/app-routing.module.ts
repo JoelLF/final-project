0d2e0ff3038767d1d64966861919ec0d6a357a9b
Maltheboss <maltheminecraft@gmail.com>
2020-03-11 09:59:54 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   { path: 'map', loadChildren: './map/map.module#MapPageModule' },
   { path: 'main', loadChildren: './main/main.module#MainPageModule' },
   { path: 'map2', loadChildren: './map2/map2.module#Map2PageModule' },
-  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+
+  { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialModule' },
   { path: 'modal', loadChildren: './modal/modal.module#ModalPageModule' },
 
 
